a63342a2ac1e8ec04090642c1294e777d1f996c3
HenSafeUP <henl@safeup.co>
2021-12-22 18:42:48 +0200

    #import <CometChatUI/PublicHeader.h>

--- CometChatUI/CometChatUI.h
+++ CometChatUI/CometChatUI.h
@@ -13,6 +13,7 @@ FOUNDATION_EXPORT double CometChatUIVersionNumber;
 //! Project version string for CometChatUI.
 FOUNDATION_EXPORT const unsigned char CometChatUIVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <CometChatUI/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like
+#import <CometChatUI/PublicHeader.h>
 
 
